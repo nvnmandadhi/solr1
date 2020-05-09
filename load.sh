b63b70c79e2f901fb97aa1561372d1b41afdc75d
naveenmandadhi <N@v33nkumar1786>
2020-05-09 17:39:19 -0500

    v1

--- load.sh
+++ load.sh
@@ -2,41 +2,45 @@
 
 set -e
 
-working_dir="$(date +"%D:%H:%M")"
-echo "Working directory is $working_dir"
-mkdir -p "$working_dir/data.json"
+curl -XDELETE -k https://localhost:9200/test -u "elastic:$ES_PASSWORD"
+curl -XPUT -k https://localhost:9200/test -u "elastic:$ES_PASSWORD"
 
-echo "Processing the data"
-rm -fr temp
-mkdir temp && pushd temp
-git clone git@github.com:CSSEGISandData/COVID-19.git
+generated_dir="$(date +%Y%m%d)"
+working_dir=$(pwd)
+echo "Data will be stored in directory $working_dir/$generated_dir"
+rm -fr "$generated_dir"
+mkdir -p "$generated_dir"
+pushd "$generated_dir" && touch data.json && popd
+
+echo "Processing the input files and generating data to index"
+pushd temp || (mkdir temp && pushd temp)
+git pull || git clone git@github.com:CSSEGISandData/COVID-19.git
 pushd ./COVID-19/csse_covid_19_data/csse_covid_19_daily_reports
+
+echo "Number of input files to process: $(ls | wc -l)"
 
 i=0
 for file in *.csv; do
   i=$((i + 1))
-  csvtojson "$file" | jq -c .[] <"$i.json" | tr -d '\r' | while read line; do
-    echo '{ "index" : {"_index" : "test" } }' >>"$working_dir/data.json"
-    echo "$line" >>"$working_dir/data.json"
+  csvtojson "$file" | jq -c .[] | tr -d '\r' | while read line; do
+    echo '{ "index" : {"_index" : "test" } }' >>"$working_dir/$generated_dir/data.json"
+    echo "$line" >>"$working_dir/$generated_dir/data.json"
   done
-  if [ $((i % 100)) -eq 0 ]; then
-    echo "Processed $i input files"
+
+  curl -u "elastic:$ES_PASSWORD" \
+    -XPOST \
+    -H 'Content-Type: application/x-ndjson' \
+    -k 'https://localhost:9200/test/_bulk?pretty' \
+    --data-binary "@$working_dir/$generated_dir/data.json" \
+    -o /dev/null \
+    -I
+
+  mv "$working_dir/$generated_dir/data.json" "$working_dir/$generated_dir/data-$i.json"
+  true >"$working_dir/$generated_dir/data.json"
+
+  if [ $((i % 10)) -eq 0 ]; then
+    echo "Processed and indexed $i input files"
   fi
 done
 
-echo "Completed processing the input files"
-echo "Indexing the data now, this takes time"
-echo "********************************************"
-
-curl -u 'elastic:f446grIQIADV99h22e2bM2T9' \
-  -o /dev/null \
-  -s -w '%{http_code}'-XPOST \
-  -I \
-  -H 'Content-Type: application/x-ndjson' \
-  -k 'https://localhost:9200/test/_bulk?pretty' \
-  --data-binary "@$working_dir/data.json"
-
-echo "********************************************"
-
-popd
-popd
+popd 2
