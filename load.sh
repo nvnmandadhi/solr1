5cc6d9168a9be81b06b8b8419ace6f8c21c5e17f
naveenmandadhi <N@v33nkumar1786>
2020-05-10 20:38:16 -0500

    simplify script

--- load.sh
+++ load.sh
@@ -2,38 +2,26 @@
 
 set -e
 
-generated_dir="$(date +%Y%m%d)"
-working_dir=$(pwd)
-echo "Data will be stored in directory $working_dir/$generated_dir"
-rm -fr "$generated_dir"
-mkdir -p "$generated_dir"
-pushd "$generated_dir" && touch data.json && popd
-
 echo "Processing the input files and generating data to index"
 git clone git@github.com:CSSEGISandData/COVID-19.git || echo "data is up-to-date"
 pushd ./COVID-19/csse_covid_19_data/csse_covid_19_daily_reports
 
-true >"$working_dir/failed.json"
 echo "Number of input files to process: $(ls | wc -l)"
 
 i=0
 for file in *.csv; do
+  touch data.json
   i=$((i + 1))
   awk 'NR==1{$0=tolower($0)} 1' "$file" \
   | sed -e '1s/ /_/g' \
   | sed -e '1s/\//_/g' \
   | csvtojson | jq -c .[] \
   | tr -d '\r' | while read line; do
-    echo "$line" >>"$working_dir/$generated_dir/data.json"
+    echo "$line" >> data.json
   done
 
-  post -c data "$working_dir/$generated_dir/data.json"
-
-  if [[ $? -ne 0 ]]; then
-   "$working_dir/$generated_dir/data.json" >> "$working_dir/failed.json"
-  fi
-
-  true >"$working_dir/$generated_dir/data.json"
+  post -c data data.json
+  rm -fr data.json
 
   if [[ $((i % 5)) -eq 0 ]]; then
     echo "Processed and indexed $i input files"
@@ -42,5 +30,3 @@ done
 
 echo "*********** Indexing complete ***********"
 popd
-# shellcheck disable=SC2115
-rm -fr "$working_dir/$generated_dir"
